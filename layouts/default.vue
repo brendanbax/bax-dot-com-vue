cd1fb1d456d0fc67bbbc5cf2502c10e5c6465cf1
Brendan Bax <brendan.bax@wwt.com>
2020-06-29 17:03:17 -0500

    swap svg theme

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,15 +27,14 @@ export default {
       if (currentTheme >= this.themeBank.length) {
         currentTheme = 0;
       }
-      this.theme = this.themeBank[currentTheme]
+      this.theme = this.themeBank[currentTheme];
     }
   },
-  mounted: {
-    generateTheme() {
-      // TODO: make this
-      // generate a random number between 0 and themeBank.length
-      // set theme to that value
-    }
+  mounted() {
+    // TODO: make this
+    // generate a random number between 0 and themeBank.length
+    // set theme to that value
+    console.log('mounted');
   }
 }
 </script>
@@ -70,7 +69,7 @@ export default {
   --bg: var(--t-lt);
   --fg: var(--t-dk);
   --font: 'Montserrat', sans-serif;
-  --hero: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cg fill='%23e7ffe7' fill-opacity='0.075'%3E%3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E");
+  --hero: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%2392c9b1' fill-opacity='0.125'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 }
 .theme-vintage {
   --t-lt: #F9F9FA;
