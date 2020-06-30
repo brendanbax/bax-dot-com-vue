67af6622c0bc82cb66f90e0fbd0516f9659ab8b9
Brendan Bax <brendanbax@baxbook-pro.lan>
2020-06-29 21:53:47 -0500

    updates nav and theme swap

--- components/IndexHero.vue
+++ components/IndexHero.vue
@@ -1,84 +1,55 @@
 <template>
   <div id="hero" class="f-dk">
-    <Logo className="logo" />
-    <div class="zip-row">
-      <span class="hero-txt txt-primary t-brand">Brendan</span>
-      <span class="hero-txt txt-primary t-brand">Bax</span>
-    </div>
-    <div class="zip-row">
-      <span class="hero-txt txt-secondary buffer t-lt">Digital</span>
-      <span class="hero-txt txt-secondary t-lt">Product</span>
-      <span class="hero-txt txt-secondary t-lt">Design</span>
-    </div>
+    <div class="hero-txt txt-primary t-brand">Brendan Bax</div>
+    <div class="hero-txt txt-secondary t-lt">Digital</div>
+    <div class="hero-txt txt-secondary t-lt">Product</div>
+    <div class="hero-txt txt-secondary t-lt">Design</div>
   </div>
 </template>
 
 <script>
-import Logo from '@/components/Logo';
-
 export default {
   name: 'IndexHero',
-  components: {
-    Logo,
-  }
 }
 </script>
 
 <style scoped>
-.zip-row {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: baseline;
-}
 #hero {
   box-sizing: border-box;
-  min-height: calc(4.5rem * 10);
-  
+  min-height: 90vh;
+  padding: var(--nav-height) 3rem 0;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
-
-  background-image: var(--hero), radial-gradient(ellipse at top, var(--t-brand), var(--t-dk));
-  background-blend-mode: luminosity;
+  background-image: var(--hero),
+                    linear-gradient(to bottom right, var(--t-brand), var(--t-dk));
+  background-blend-mode: unset;
   background-repeat: repeat;
 }
 .txt-primary {
-  font-size: 8rem;
-  line-height: 8rem;
-}
-.txt-primary:first-child {
-  margin-right: 2rem;
+  font-size: 4.5rem;
+  line-height: 4.5rem;
 }
 .txt-secondary {
-  font-size: 4rem;
-  line-height: 5rem;
-}
-:not(.txt-secondary:first-child) .txt-secondary{
-  margin-left: 2rem;
+  font-size: 3rem;
+  line-height: 3rem;
 }
 .hero-txt {
   font-family: var(--font);
-  text-align: center;
   text-transform: uppercase;
   font-weight: bolder;
-  letter-spacing: -.25rem;
   transform: translateX(-4px);
-  text-shadow: 0 1.1rem 2rem var(--t-dk),
-              0 .25rem .75rem rgba(0,0,0,0.2);
+  text-shadow: .75rem .75rem 1.5rem var(--t-dk),
+              .5rem .5rem .75rem rgba(0,0,0,0.2),
+              -1px -1px 1px var(--t-lt);
 }
 .hero-txt::after {
   content: inherit;
   display: block;
 }
 .logo {
-  position: absolute;
-  left: 50%;
-  top: 30%;
-  transform: translate(-50%, -50%);
-  height: 50%;
-  z-index: 0;
+  width: 200px;
+  height: 200px;
   fill: none;
   stroke: var(--t-brand);
   stroke-width: 3px;
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,22 +1,98 @@
 <template>
   <nav>
-    <nuxt-link to="/">About</nuxt-link>
-    <nuxt-link to="/projects">Projects</nuxt-link>
+    <div class="logo-container" @click="swap" tabindex="0">
+      <Logo class="nav-logo"/>
+    </div>
+    <span>
+      <nuxt-link class="nav-link" to="/">About</nuxt-link>
+      <nuxt-link class="nav-link" to="/projects">Projects</nuxt-link>
+    </span>
   </nav>
 </template>
 
 <script>
+  import Logo from '@/components/Logo';
+
   export default {
     name: 'NavBar',
+    components: {
+      Logo,
+    },
+    methods: {
+      swap() {
+        this.$emit('swap');
+      }
+    }
   }
 </script>
 
 <style scoped>
-  nav {
-    height: var(--nav-height);
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-  }
+nav {
+  height: var(--nav-height);
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 1rem 3rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.logo-container {
+  box-sizing: border-box;
+  display: flex;
+  position: relative;
+  z-index: 999;
+  justify-content: center;
+  align-items: center;
+  padding: .5rem;
+  background: var(--t-dk);
+  border-radius: .25rem;
+  box-shadow: .5rem .5rem 1rem -.25rem var(--t-dk),
+              .125rem .125rem .5rem rgba(0,0,0,0.2);
+  cursor: pointer;
+  transition: border-radius 200ms;
+  border: 2px solid transparent;
+}
+.logo-container:hover,
+.logo-container:focus {
+  border-radius: 50%;
+  transition: border-radius 200ms;
+}
+.logo-container:focus {
+  outline: none;
+  border: 2px solid var(--t-brand-lt);
+}
+.logo-container::after {
+  content: "Theme";
+  position: absolute;
+  bottom: 0;
+  width: max-content;
+  
+  color: var(--t-lt);
+  font-family: var(--font);
+  font-size: .75rem;
+  text-transform: uppercase;
+  opacity: 0;
+}
+.logo-container:hover::after {
+  bottom: -1.5rem;
+  z-index: 0;
+  opacity: 1;
+  transition: bottom 200ms 500ms, opacity 200ms 500ms;
+}
+.nav-logo {
+  width: 1.5rem;
+  height: 1.5rem;
+  fill: var(--t-lt);
+}
+.nav-link {
+  color: var(--t-lt);
+  font-family: var(--font);
+  text-decoration: none;
+  font-size: 1rem;
+}
+.nav-link:first-child {
+  margin-right: 2rem;
+}
 </style>
