1e1bc71b50b835c5d11ff3ac3f8e88d65bafb008
Brendan Bax <brendan.bax@wwt.com>
2020-07-06 14:56:17 -0500

    fix font

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,8 @@ export default {
       { hid: 'description', name: 'description', content: 'Brendan is a passionate designer and developer building products that intersect practical design and cutting-edge technology.  He applies his skills at startups, agencies, and major enterprises using design thinking, rapid prototyping, and agile development to create user-centered solutions for complex business problems.' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;900&display=swap' }
     ]
   },
   css: ['~/assets/animation.css'],
