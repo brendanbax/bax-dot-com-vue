ab1c0425751aad94f7ddba9bc9ed1d54bf385a4b
Brendan Bax <brendan.bax@wwt.com>
2020-07-06 14:49:42 -0500

    update site desc.

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Excellent description of my site here...' }
+      { hid: 'description', name: 'description', content: 'Brendan is a passionate designer and developer building products that intersect practical design and cutting-edge technology.  He applies his skills at startups, agencies, and major enterprises using design thinking, rapid prototyping, and agile development to create user-centered solutions for complex business problems.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
